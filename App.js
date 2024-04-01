67f599d50a96a18436bb8e57e023382f9be7bfdf
RajRn12 <raj.rai@lethbridgecollege.ca>
2024-03-31 20:10:10 -0600

    b

--- App.js
+++ App.js
@@ -54,6 +54,200 @@ export default function App() {
         })
     }, [])
 
+    // recordings
+    const startRecording = async () => {
+        try {
+            // request permission to use the mic
+            if (permissionsResponse.status !== 'granted') {
+                console.log('Requesting permissions.');
+                await requestPermission();
+            }
+            console.log('Permission is ', permissionsResponse.status);
+
+            await Audio.setAudioModeAsync({
+                allowsRecordingIOS: true,
+                playsInSilentModeIOS: true,
+            });
+
+            console.log('Starting recording...');
+            const { recording } = await Audio.Recording.createAsync(
+                Audio.RecordingOptionsPresets.HIGH_QUALITY
+            );
+            setRecording(recording);
+            console.log('...recording');
+        }
+        catch (errorEvent) {
+            console.error('Failed to startRecording(): ', errorEvent);
+        }
+    }
+
+    const stopRecording = async () => {
+        try {
+            // stop the actual recording
+            await recording.stopAndUnloadAsync();
+
+            // save the recorded object location
+            const uri = (recording.getURI());
+
+            // save uri
+            addRecording(uri)
+
+            // forget the recording object
+            setRecording(undefined);
+
+            // log the result
+            console.log('Recording stopped and stored at ', uri);
+
+        }
+        catch (errorEvent) {
+            console.error('Failed to stopRecording(): ', errorEvent);
+        }
+    }
+
+    useEffect(() => {
+        return recording
+            ? () => {
+                recording.stopAndUnloadAsync()
+            }
+            : undefined;
+    }, [])
+
+    // database
+    const [recordings, setRecordings] = useState([]);
+    const [updateRecordings, forceUpdate] = useState(0);
+
+    // database starts from id value of 1 -
+    const [recordedList, setRecordedList] = useState([
+        { id: 0, sound: null, play: false, bgColor: '' }, { id: 1, sound: null, play: false, bgColor: '' }, { id: 2, sound: null, play: false, bgColor: '' }, { id: 3, sound: null, play: false, bgColor: '' }, { id: 4, sound: null, play: false, bgColor: '' },
+        { id: 5, sound: null, play: false, bgColor: '' }, { id: 6, sound: null, play: false, bgColor: '' }, { id: 7, sound: null, play: false, bgColor: '' }, { id: 8, sound: null, play: false, bgColor: '' }, { id: 9, sound: null, play: false, bgColor: '' },
+        { id: 10, sound: null, play: false, bgColor: '' }, { id: 11, sound: null, play: false, bgColor: '' }, { id: 12, sound: null, play: false, bgColor: '' }, { id: 13, sound: null, play: false, bgColor: '' }, { id: 14, sound: null, play: false, bgColor: '' },
+        { id: 15, sound: null, play: false, bgColor: '' }, { id: 16, sound: null, play: false, bgColor: '' }, { id: 17, sound: null, play: false, bgColor: '' }, { id: 18, sound: null, play: false, bgColor: '' }, { id: 19, sound: null, play: false, bgColor: '' },
+        { id: 20, sound: null, play: false, bgColor: '' },
+    ])
+
+    //update when the database  changes [db, updateRecordings]
+    useEffect(() => {
+        if (db != null) {
+            db.transaction(
+                (tx) => {
+                    tx.executeSql(
+                        "select * from recordings limit 20",
+                        [],
+                        (_, { rows }) => setRecordings(rows._array),
+                        (_, error) => console.log(error)
+                    ),
+                        (_, error) => console.log(error),
+                        () => console.log("retrieving * from recordings")
+                }
+            )
+        }
+        chooseRecordColor()
+    }, [db, updateRecordings])
+
+    const addRecording = (uri) => {
+        db.transaction(
+            (tx) => {
+                tx.executeSql(
+                    "insert into recordings (uri) values (?)",
+                    [uri],
+                    () => console.log("added", uri),
+                    (_, error) => console.log(error)
+                )
+            },
+            (_, error) => console.log('addRecording() failed: ', error),
+            forceUpdate(f => f + 1)
+        )
+    }
+
+    const deleteRecord = (id) => {
+        stopPlayingRecording(id);
+        db.transaction(
+            (tx) => {
+                tx.executeSql(
+                    "delete from recordings where id = ?",
+                    [id],
+                    () => console.log("deleted record ", id),
+                    (_, error) => console.log(error)
+                )
+            },
+            (_, error) => console.log('deleteRecord() failed: ', error),
+            forceUpdate(f => f + 1)
+        )
+        let newR = { ...recordedList }
+        newR[id].sound = null; newR[id].play = false; newR[id].bgColor = '';
+        setRecordedList(newR);
+    }
+
+    const loadRecording = async (recordedS, id) => {
+        chooseRecordColor(id);
+        const { sound } = await Audio.Sound.createAsync({ uri: recordedS });
+        await sound.setIsLoopingAsync(true)
+
+        let newR = { ...recordedList }
+        newR[id].sound = sound;
+        setRecordedList[newR]
+        console.log("loaded Recording ", id)
+        playRecording(id)
+    }
+
+    const unloadRecorded = async () => {
+        let x = 0;
+        while (x < recordedList.length) {
+            // stop and unload
+            if (recordedList[x].sound != null) {
+                await recordedList[x].sound.stopAsync();
+                await recordedList[x].sound.unloadAsync();
+                let playA = { ...recordedList }
+                playA[x].play = false;
+                recordedList(playA)
+                console.log("unloaded recording", x)
+            }
+            x++
+        }
+    }
+
+    // choose random color for recorded sound box
+    function chooseRecordColor() {
+        let i = 0;
+        let newColor = { ...recordedList }
+        while (i < recordedList.length) {
+            if (recordedList[i].sound == null && recordedList[i].bgColor == '') {
+                let r = Math.floor(Math.random() * 5);
+                newColor[i].bgColor = fullColors[r];
+                setRecordedList[newColor];
+            }
+            i++;
+        }
+    }
+
+    const playRecording = async (id) => {
+        let newRPlay = { ...recordedList }
+        if (newRPlay[id].sound != null && newRPlay[id].play == false) {
+            console.log("Playing Recording", id)
+            await recordedList[id].sound.playAsync();
+            newRPlay[id].play = true;
+            setRecordedList[newRPlay]
+        }
+    }
+
+    const stopPlayingRecording = async (id) => {
+        let newRStop = { ...recordedList }
+        if (newRStop[id].sound != null && newRStop[id].play == true) {
+            console.log("Stopped Playing Recording", id)
+            await recordedList[id].sound.stopAsync();
+            newRStop[id].play = false;
+            setRecordedList[newRStop]
+        }
+    }
+
+    useEffect(() => {
+        return recordedList
+            ? () => {
+                unloadRecorded()
+            }
+            : undefined;
+    }, [recordedList.sound])
+
     // Sounds
     const theme = require('./assets/sfx/theme.mp3');
     const tabla = require('./assets/sfx/TablaBeat.mp3');
@@ -180,200 +374,6 @@ export default function App() {
             }
             : undefined;
     }, [soundList.music])
-
-    // recordings
-    const startRecording = async () => {
-        try {
-                // request permission to use the mic
-                if (permissionsResponse.status !== 'granted') {
-                    console.log('Requesting permissions.');
-                    await requestPermission();
-                }
-                console.log('Permission is ', permissionsResponse.status);
-
-                await Audio.setAudioModeAsync({
-                    allowsRecordingIOS: true,
-                    playsInSilentModeIOS: true,
-                });
-
-                console.log('Starting recording...');
-                const { recording } = await Audio.Recording.createAsync(
-                    Audio.RecordingOptionsPresets.HIGH_QUALITY
-                );
-                setRecording(recording);
-                console.log('...recording');
-        }
-        catch (errorEvent) {
-            console.error('Failed to startRecording(): ', errorEvent);
-        }
-    }
-
-    const stopRecording = async () => {
-        try {
-            // stop the actual recording
-            await recording.stopAndUnloadAsync();
-
-            // save the recorded object location
-            const uri = (recording.getURI());
-
-            // save uri
-            addRecording(uri)
-
-            // forget the recording object
-            setRecording(undefined);
-
-            // log the result
-            console.log('Recording stopped and stored at ', uri);
-
-        }
-        catch (errorEvent) {
-            console.error('Failed to stopRecording(): ', errorEvent);
-        }
-    }
-
-    useEffect(() => {
-        return recording
-            ? () => {
-                recording.stopAndUnloadAsync()
-            }
-            : undefined;
-    }, [])
-
-    // database
-    const [recordings, setRecordings] = useState([]);
-    const [updateRecordings, forceUpdate] = useState(0);
-
-    // database starts from id value of 1 -
-    const [recordedList, setRecordedList] = useState([
-        { id: 0, sound: null, play: false, bgColor: '' }, { id: 1, sound: null, play: false, bgColor: '' }, { id: 2, sound: null, play: false, bgColor: '' }, { id: 3, sound: null, play: false, bgColor: '' }, { id: 4, sound: null, play: false, bgColor: '' },
-        { id: 5, sound: null, play: false, bgColor: '' }, { id: 6, sound: null, play: false, bgColor: '' }, { id: 7, sound: null, play: false, bgColor: '' }, { id: 8, sound: null, play: false, bgColor: '' }, { id: 9, sound: null, play: false, bgColor: '' },
-        { id: 10, sound: null, play: false, bgColor: '' }, { id: 11, sound: null, play: false, bgColor: '' }, { id: 12, sound: null, play: false, bgColor: '' }, { id: 13, sound: null, play: false, bgColor: '' }, { id: 14, sound: null, play: false, bgColor: '' },
-        { id: 15, sound: null, play: false, bgColor: '' }, { id: 16, sound: null, play: false, bgColor: '' }, { id: 17, sound: null, play: false, bgColor: '' }, { id: 18, sound: null, play: false, bgColor: '' }, { id: 19, sound: null, play: false, bgColor: '' },
-        { id: 20, sound: null, play: false, bgColor: '' },
-    ])
-
-    //update when the database  changes [db, updateRecordings]
-    useEffect(() => {
-        if (db != null) {
-            db.transaction(
-                (tx) => {
-                    tx.executeSql(
-                        "select * from recordings limit 20",
-                        [],
-                        (_, { rows }) => setRecordings(rows._array),
-                        (_, error) => console.log(error)
-                    ),
-                        (_, error) => console.log(error),
-                        () => console.log("retrieving * from recordings")
-                }
-            )
-        }
-        chooseRecordColor()
-    }, [db, updateRecordings])
-
-    const addRecording = (uri) => {
-            db.transaction(
-                (tx) => {
-                    tx.executeSql(
-                        "insert into recordings (uri) values (?)",
-                        [uri],
-                        () => console.log("added", uri),
-                        (_, error) => console.log(error)
-                    )
-                },
-                (_, error) => console.log('addRecording() failed: ', error),
-                forceUpdate(f => f + 1)
-            )
-    }
-
-    const deleteRecord = (id) => {
-        stopPlayingRecording(id);
-        db.transaction(
-            (tx) => {
-                tx.executeSql(
-                    "delete from recordings where id = ?",
-                    [id],
-                    () => console.log("deleted record ", id),
-                    (_, error) => console.log(error)
-                )
-            },
-            (_, error) => console.log('deleteRecord() failed: ', error),
-            forceUpdate(f => f + 1)
-        )
-        let newR = { ...recordedList }
-        newR[id].sound = null; newR[id].play = false; newR[id].bgColor = '';
-        setRecordedList(newR);
-    }
-
-    const loadRecording = async (recordedS, id) => {
-        chooseRecordColor(id);
-        const { sound } = await Audio.Sound.createAsync({ uri: recordedS });
-        await sound.setIsLoopingAsync(true)
-
-        let newR = { ...recordedList }
-        newR[id].sound = sound;
-        setRecordedList[newR]
-        console.log("loaded Recording ", id)
-        playRecording(id)
-    }
-
-    const unloadRecorded = async () => {
-        let x = 0;
-        while (x < recordedList.length) {
-            // stop and unload
-            if (recordedList[x].sound != null) {
-                await recordedList[x].sound.stopAsync();
-                await recordedList[x].sound.unloadAsync();
-                let playA = { ...recordedList }
-                playA[x].play = false;
-                recordedList(playA)
-                console.log("unloaded recording", x)
-            }
-            x++
-        }
-    }
-
-    // choose random color for recorded sound box
-    function chooseRecordColor() {
-        let i = 0;
-        let newColor = { ...recordedList }
-        while (i < recordedList.length) {
-            if (recordedList[i].sound == null && recordedList[i].bgColor == '') {
-                let r = Math.floor(Math.random() * 5);
-                newColor[i].bgColor = fullColors[r];
-                setRecordedList[newColor];
-            }
-            i++;
-        }
-    }
-
-    const playRecording = async (id) => {
-        let newRPlay = { ...recordedList }
-        if (newRPlay[id].sound != null && newRPlay[id].play == false) {
-            console.log("Playing Recording", id)
-            await recordedList[id].sound.playAsync();
-            newRPlay[id].play = true;
-            setRecordedList[newRPlay]
-        }
-    }
-
-    const stopPlayingRecording = async (id) => {
-        let newRStop = { ...recordedList }
-        if (newRStop[id].sound != null && newRStop[id].play == true) {
-            console.log("Stopped Playing Recording", id)
-            await recordedList[id].sound.stopAsync();
-            newRStop[id].play = false;
-            setRecordedList[newRStop]
-        }
-    }
-
-    useEffect(() => {
-        return recordedList
-            ? () => {
-                unloadRecorded()
-            }
-            : undefined;
-    }, [recordedList.sound])
 
     return (
         <View style={styles.container}>
